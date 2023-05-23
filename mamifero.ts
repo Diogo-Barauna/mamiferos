901f780a0c10d697d063159505cb675231e38684
Diogo <diogobarauna233@gmail.com>
2023-05-23 16:18:07 -0300

    Adicionando a função PUT/UPDATE

--- mamifero.ts
+++ mamifero.ts
@@ -12,40 +12,73 @@ interface Elefante {
     saudavel: boolean
   }
   
+// Função de cadastro
 app.post('/reg', async (request: FastifyRequest <{ Body: Elefante }>, reply: FastifyReply) => {
-  
+
     const { nome, sexo, idade, saudavel } = request.body 
 
-    const elefante = await prisma.elefante.create({
+    try {
+      const elefante = await prisma.elefante.create({
+        data: {
+          nome,
+          sexo,
+          idade,
+          saudavel
+        },
+      });
+
+      return { message: 'Elefante cadastrado com sucesso!'};
+
+    } catch(error) {
+      reply.status(500).send({ error: 'Ocorreu um erro ao cadastrar o elefante.' });
+    }  
+  });
+
+// Função de delete
+app.delete('/del', async (request: FastifyRequest<{ Querystring: { nome: string } }>, reply: FastifyReply) => {
+
+  const { nome } = request.query;
+  
+  try {
+    await prisma.elefante.deleteMany({
+      where: {
+        nome
+      }
+    });
+
+    return { message: 'Elefante excluído com sucesso!' };
+
+  } catch (error) {
+    reply.status(500).send({ error: 'Ocorreu um erro ao excluir o elefante.' });
+  }
+  });
+// Função de atualização
+app.put('/upd/:nome', async (request: FastifyRequest<{ Params: { nome: string }, Body: Elefante }>, reply: FastifyReply) => {
+
+  const { nome } = request.params;
+  const { sexo, idade, saudavel } = request.body;
+  
+  try {
+    const elefante = await prisma.elefante.update({
+      where: {
+        nome
+      },
       data: {
-        nome,
         sexo,
         idade,
         saudavel
-      },
+      }
     });
   
-    return { message: 'Elefante cadastrado com sucesso!'};
-  });
-
-  app.delete('/del', async (request: FastifyRequest<{ Querystring: { nome: string } }>, reply: FastifyReply) => {
-    const { nome } = request.query;
-  
-    try {
-      await prisma.elefante.deleteMany({
-        where: {
-          nome: nome
-        }
-      });
-
-      return { message: 'Elefante excluído com sucesso!' };
+    return { message: 'Elefante atualizado com sucesso!'};
+    
     } catch (error) {
-      reply.status(500).send({ error: 'Ocorreu um erro ao excluir os elefantes.' });
+      reply.status(500).send({ error: 'Ocorreu um erro ao atualizar o elefante.' });
     }
   });
+  
+app.register(cors, {
+  origin: "*",
+});
 
-  app.register(cors, {
-    origin: "*",
-  });
-
-  app.listen({port:3333})
+app.listen({port:3333})
