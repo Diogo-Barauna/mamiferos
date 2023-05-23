c85e0fe871f4daf0e0efa9358accae2b36fabb39
Diogo <diogobarauna233@gmail.com>
2023-05-23 15:00:08 -0300

    Adicionando a função 'POST' ou 'CREATE' e adicionando uma interface para Elefante

--- mamifero.ts
+++ mamifero.ts
@@ -1,7 +1,35 @@
-import fastify from "fastify";
+import fastify, { FastifyRequest, FastifyReply } from "fastify";
+import cors from '@fastify/cors'
 import { PrismaClient } from "@prisma/client";
 
 const app = fastify()
 const prisma = new PrismaClient()
 
+interface Elefante {
+    nome: string;
+    sexo: string;
+    idade: number;
+    saudavel: boolean
+  }
+  
+app.post('/reg', async (request: FastifyRequest <{ Body: Elefante }>, reply: FastifyReply) => {
+  
+    const { nome, sexo, idade, saudavel } = request.body 
 
+    const elefante = await prisma.elefante.create({
+      data: {
+        nome,
+        sexo,
+        idade,
+        saudavel
+      },
+    });
+  
+    return { message: 'Elefante cadastrado com sucesso!'};
+  });
+
+  app.register(cors, {
+    origin: "*",
+  });
+
+  app.listen({port:3333})
