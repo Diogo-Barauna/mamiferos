3f88050e535753a84c78b99d780cb01ea923161c
Diogo <diogobarauna233@gmail.com>
2023-06-01 15:24:31 -0300

    Adicionando quatro botões com os textos 'cadastrar', 'pesquisar','editar' e 'deletar' e adicionando o título 'banco de elefantes'

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,5 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
@@ -7,7 +8,22 @@ export default function App() {
         source={require('./assets/logo.png')}
         style={styles.logo}
         />
-    </View>
+        <Text style={styles.txtLogo}>Banco de elefantes</Text>
+        <View style={styles.btns}>
+          <TouchableOpacity style={styles.button}>
+              <Text>Cadastrar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+              <Text>Pesquisar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+              <Text>Editar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+              <Text>Deletar</Text>
+          </TouchableOpacity>
+        </View>
+  </View>
   );
 }
 
@@ -21,9 +37,24 @@ const styles = StyleSheet.create({
     width:375
   },
   logo: {
-    top: 40,
+    top: 30,
     height:80,
     width:80,
     position: 'absolute'
+  },
+  txtLogo:{
+    fontSize:35,
+  },
+  btns: {
+    top: 40,
+    flexDirection: 'column',
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height:50,
+    width:170,
+    border:'solid',
+    marginTop: 40
   }
 });
